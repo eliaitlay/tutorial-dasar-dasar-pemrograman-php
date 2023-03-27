a32868d7d8b8c85711125d052c43ea54b0b8fc71
Elia itlay <itlayelia@gmail.com>
2023-03-28 02:30:52 +0900

    tambah echo-print.php

---
+++ echo-print.php
@@ -0,0 +1,12 @@
+<?php
+
+    // Contoh penggunaan echo
+    echo "<h1>Papua Tech Community</h1>";
+
+    echo "<p>Berbagi dan belajar bersama di komunitas.</p>";
+
+    // Contoh penguna print
+    print "<h1>Papua Coding School</h1>";
+    print "<p>Belajar coding dari dasar dengan mengikuti kursus.</p>";
+
+?>
