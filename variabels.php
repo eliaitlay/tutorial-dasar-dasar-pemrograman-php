b4259d25ee8c38f79d855a017e9f528307fe7a9b
Elia itlay <itlayelia@gmail.com>
2023-03-28 01:51:33 +0900

    tambah variabels.php

---
+++ variabels.php
@@ -0,0 +1,15 @@
+<?php
+
+    // contoh penulisan variabels didalam file PHP
+    $title = "SaCode";
+    $tag_line = "Papua Coding School";
+    $tahun = "2022";
+
+    // Menampilkan data dari dalam variabel
+    echo $title;
+    echo "<br>";
+    echo $tag_line;
+    echo "<br>";
+    echo $tahun;
+
+?>
